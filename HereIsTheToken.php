6e28d37b5d78614c97877a55578bed759bd054e0
Winzmcman <nikithobbi@gmail.com>
2020-04-16 19:27:39 +0300

    alpha1.0

--- HereIsTheToken.php
+++ HereIsTheToken.php
@@ -1,6 +1,6 @@
 <?php
 
-define('URL', "https://api.telegram.org/bot1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0/");
+define('URL', "https://api.telegram.org/botHereIsTheToken/");
 require('classes/Bot.php');
 
 $json = file_get_contents('php://input');
