aebbbbdabc00188a5f10bebe5ee58704a75f440c
Winzmcman <nikithobbi@gmail.com>
2020-04-16 18:39:14 +0300

    g

--- 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
+++ 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
@@ -8,11 +8,13 @@ $data = json_decode($json);
 $triggers = require('triggers.php');
 $answers = require('answers.php');
 
+$maxKeyAnswers = max(array_keys($answers));
+
 $bot = new Bot(URL);
 foreach($triggers as $trigger){
     if(isset($data->message->text) && $data->message->text != '/start' && $data->message->text == $trigger){
         $chat_id = $data->message->chat->id;
-        $text = $answers[rand(0,3)];
+        $text = $answers[rand(0,$maxKeyAnswers)];
         $bot->do('sendMessage', "chat_id=$chat_id&text=$text");
     }
 }
