1380ab38b2eb3f4254564be87a8b0594b4e187d0
Winzmcman <nikithobbi@gmail.com>
2020-04-16 17:54:30 +0300

    t10

--- 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
+++ 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
@@ -2,6 +2,7 @@
 
 define('TOKEN', "1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0");
 define('URL', "https://api.telegram.org/bot1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0/");
+require('classes/Bot.php');
 
 $json = file_get_contents('php://input');
 $data = json_decode('{}'); 
