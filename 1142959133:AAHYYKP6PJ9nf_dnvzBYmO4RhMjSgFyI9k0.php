78b8d7c0a107e9f406b53eb680b54fcfe58f4bda
Winzmcman <nikithobbi@gmail.com>
2020-04-16 17:45:36 +0300

    t3

--- 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
+++ 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
@@ -4,10 +4,11 @@ define('TOKEN', "1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0");
 define('URL', "https://api.telegram.org/bot1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0/");
 
 $json = file_get_contents('php://input');
-$data = json_decode($json);
+$data = new class(){}
 
 $bot = new Bot(URL);
 
 if(isset($data->message->text) && $data->message->text != '/start'){
-    $bot->do('SendMessage');
+    $chat_id = $data->message->chat->id;
+    $bot->do('SendMessage', "chat_id=$chat_id&text=Пошел ты нахуй мусор");
 }
