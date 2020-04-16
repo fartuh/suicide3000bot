4d110a928548be0cbc2fa30c6ad867b02c446282
Winzmcman <nikithobbi@gmail.com>
2020-04-16 17:42:53 +0300

    t2

--- 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
+++ 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 define('TOKEN', "1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0");
 define('URL', "https://api.telegram.org/bot1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0/");
 
@@ -13,5 +9,5 @@ $data = json_decode($json);
 $bot = new Bot(URL);
 
 if(isset($data->message->text) && $data->message->text != '/start'){
-    $bot->do('SendMessage')
+    $bot->do('SendMessage');
 }
