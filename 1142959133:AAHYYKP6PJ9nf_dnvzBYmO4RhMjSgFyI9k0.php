c55fcb980a3a1c4c6e00d810147970a28d3b1b46
Winzmcman <nikithobbi@gmail.com>
2020-04-16 18:05:29 +0300

    a

--- 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
+++ 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
@@ -1,6 +1,6 @@
 <?php
 
-define('URL', "https://api.telegram.org/botHereIsTheToken/");
+define('URL', "https://api.telegram.org/bot1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0/");
 require('classes/Bot.php');
 
 $json = file_get_contents('php://input');
@@ -9,5 +9,5 @@ $data = json_decode($json);
 $bot = new Bot(URL);
 if(isset($data->message->text) && $data->message->text != '/start'){
     $chat_id = $data->message->chat->id;
-    $bot->do('sendMessage', "chat_id=$chat_id&text=Привет");
+    $bot->do('sendMessage', "chat_id=$chat_id&text=Пошел ты нахуй мусор");
 }
