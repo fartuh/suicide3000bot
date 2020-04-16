cb87827ae10a6d16e0af4298f09956b8febcad84
Winzmcman <nikithobbi@gmail.com>
2020-04-16 19:18:58 +0300

    g

--- 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
+++ 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
@@ -9,11 +9,13 @@ $triggers = require('triggers.php');
 $answers = require('answers.php');
 
 $bot = new Bot(URL, $triggers, $answers);
-if(isset($data->message->text)){
+//if(isset($data->message->text)){
     $trigger = $bot->checkTrigger($data->message->text);
+    if($trigger){
         $text = $bot->getAnswer();
 
         $chat_id = $data->message->chat->id;
         $message_id = $data->message->message_id;
-        $bot->do('sendMessage', "chat_id=$chat_id&text=hi&reply_to_message_id=$message_id");
-}
+        $bot->do('sendMessage', "chat_id=$chat_id&text=$text&reply_to_message_id=$message_id");
+    }
+//}
