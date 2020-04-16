4e3676ecd4f160071009ee1656138221674795c1
Winzmcman <nikithobbi@gmail.com>
2020-04-16 18:30:34 +0300

    f

--- 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
+++ 1142959133:AAHYYKP6PJ9nf_dnvzBYmO4RhMjSgFyI9k0.php
@@ -6,11 +6,13 @@ require('classes/Bot.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json); 
 $triggers = require('triggers.php');
+$answers = require('answers.php');
 
 $bot = new Bot(URL);
 foreach($triggers as $trigger){
     if(isset($data->message->text) && $data->message->text != '/start' && $data->message->text == $trigger){
         $chat_id = $data->message->chat->id;
-        $bot->do('sendMessage', "chat_id=$chat_id&text=Суицид - выход");
+        $text = $answers[rand(0,5)];
+        $bot->do('sendMessage', "chat_id=$chat_id&text=$text");
     }
 }
